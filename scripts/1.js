d941b7fc92327aa32ef0134f77d4cb5f2897aaf1
ximenaortegam2605@gmail.com <laurameliza26@gmail.com>
2023-08-10 10:07:32 -0500

    celular

--- scripts/1.js
+++ scripts/1.js
@@ -1,3 +1,3 @@
 let cedula = prompt("ingrese su cedula");
 let celular = prompt("ingrese su numero de celular");
-console.log("los datos de este usuario son: " + cedula + celular)
+console.log("los datos de este usuario son: " + cedula + celular);
